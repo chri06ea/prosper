b7a325e229402def30bb308ad4e219238fc2a837
Christian <chri06ea@edu.ucl.dk>
2022-12-24 18:03:59 +0100

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,6 @@
-#include "core.hpp"
-#include "game.hpp"
-#include "platform.hpp"
-#include "renderer.hpp"
-
-using namespace prosper;
-using namespace prosper::platform;
+#include "prosper.hpp"
 
 int main()
 {
-    if (!initialize_platform())
-    {
-        emergency_shutdown("platform initialization failed");
-    }
-
-    GameState gs;
-
-    if (!initialize_gamestate(gs))
-    {
-        emergency_shutdown("GameState initialization failed");
-    }
-
-    auto renderer = create_renderer();
-
-    if (!renderer)
-    {
-        emergency_shutdown("create_renderer initialization failed");
-    }
-
-    while (true)
-    {
-        process_input();
-
-        update_gamestate(gs);
-
-        render_gamestate(gs, *renderer);
-    }
-
-    return 0;
+    return prosper::prosper_main();
 }
---
+++ src/prosper.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace prosper
+{
+    int prosper_main();
+}
