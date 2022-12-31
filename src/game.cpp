9f031e0271c791144ca1560bfc2f7bb555fa1ca0
Christian <chri06ea@edu.ucl.dk>
2022-12-31 16:56:25 +0100

    Drawing ground surface!

--- src/game.cpp
+++ src/game.cpp
@@ -12,25 +12,16 @@ namespace prosper
 
         // Draw scenary (pretty much the background)
 
-        renderer->draw({.vertices = {{
-                            // Center box
-                            {-0.25f, -0.25f, 0.f}, // Bottom left
-                            {0.25f, -0.25f, 0.f},  // Bottom right
-                            {0.25f, 0.25f, 0.f},   // Top right
-                            {-0.25f, 0.25f, 0.f},  // Top left
-                        }}});
-
-        renderer->draw({.vertices{{
-            // Top right box
-            {-0.25f + 0.75f, -0.25f + 0.75f, 0.f}, // Bottom left
-            {0.25f + 0.75f, -0.25f + 0.75f, 0.f},  // Bottom right
-            {0.25f + 0.75f, 0.25f + 0.75f, 0.f},   // Top right
-            {-0.25f + 0.75f, 0.25f + 0.75f, 0.f},  // Top left
-        }}});
-
         // Draw sun
 
         // Draw ground
+
+        renderer->draw({.vertices = {{
+                            {-1.00f, -1.00f, 0.f}, // Bottom left
+                            {+1.00f, -1.00f, 0.f}, // Bottom right
+                            {+1.00f, -0.25f, 0.f}, // Top right
+                            {-1.00f, -0.25f, 0.f}, // Top left
+                        }}});
 
         // Draw buildings
 
